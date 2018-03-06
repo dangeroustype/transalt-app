3912930720f821ce33c9ffd3bce6113bd7dd91da
dangeroustype <arthur@arthurhanna.com>
2018-03-06 17:45:37 -0500

    Beginnings of Get Involved screen

--- TransAlt/components/GetInvolved.js
+++ TransAlt/components/GetInvolved.js
@@ -4,11 +4,18 @@ import {
   View,
   WebView,
   TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  Dimensions,
+  Image
 } from "react-native";
 
 import { NavigationActions } from "react-navigation";
 
 import Icon from "react-native-vector-icons/MaterialIcons";
+
+const win = Dimensions.get('window');
+const ratio = win.width/1140; //541 is actual image width
 
 class GetInvolved extends Component {
 
@@ -27,18 +34,82 @@ class GetInvolved extends Component {
   render() {
 
     return (
+      <ScrollView style={styles.container}>
+
+      <Image
+      style={styles.image}
+       resizeMode={'contain'}
+
+      source={require('../assets/img/getinvolved.png')}
+       />
+
+      <Text style={styles.firstparagraph}>You can make real, tangible changes to how streets and sidewalks function in your Neighborhood. Your local activist committee campaigns and fights for changes on-the-ground in their neighborhoods, like bike lanes and new pedestrian plazas. </Text>
+
+      <Text>
+      Choose your committee
+      </Text>
+
+      <Text>
+      Manhattan
+      </Text>
+
+      <Text>
+      Brooklyn
+      </Text>
+
+      <Text>
+      Queens
+      </Text>
+
+      <Text>
+      the Bronx
+      </Text>
+      <Text>
+      Staten Island
+      </Text>
 
 
-      <WebView
-  source={{uri: 'https://www.transalt.org/getinvolved'}}
-  style={{marginTop: 20}}
-  />
 
+<WebView
+source={{uri: 'https://www.transalt.org/getinvolved'}}
+style={{marginTop: 20}}
+/>
 
+</ScrollView>
     )
 
   }
 
 }
+
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  textcontainer: {
+    paddingTop: 10,
+  paddingHorizontal: 10,
+
+  },
+  firstparagraph: {
+
+    color: '#0d9c4a',
+    fontSize: 18,
+    padding: 10,
+  },
+
+  paragraph: {
+    color: '#363636',
+    fontSize: 16,
+    paddingBottom: 10,
+  },
+  image: {
+    width: win.width,
+    height: 650 * ratio, //362 is actual height of image
+}
+
+});
 
 export default GetInvolved;
